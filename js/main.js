04c0d235a366ea9f499505e97a498e568a568168
frsmtv <fsimitchiev@gmail.com>
2017-04-12 01:00:56 +0200

    ooo

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  // WATER RIPPLES
+  $('body').ripples({
+    dropRadius: 20,
+    perturbance: 0.03,
+    interactive: true,
+    resolution: 512,
+  });
+
+
+});
